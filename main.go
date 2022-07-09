0e66fa9cb3d51d5fc0da84d77f9fdf184d20e3df
bigvini <98239700+bigvini@users.noreply.github.com>
2022-07-09 12:39:28 +0300

    Add files via upload
    Sum of Digits / Digital Root

--- main.go
+++ main.go
@@ -7,49 +7,43 @@ import (
 )
 
 func main() {
-	input := "1 9 3 4 -5"
-
-	var result string
-	numberInput := []int32{}
-
-	stroka := strings.Split(input, " ")
-	var max int32 = 0
-	var min int32 = 0
-
-	for _, valueInput := range stroka {
-		num, err := strconv.Atoi(valueInput)
-		if err == nil {
-			numberInput = append(numberInput, int32(num))
-		}
-	}
-
-	for _, value := range numberInput {
-		if max == 0 {
-			max = value
-		}
-
-		if value > max {
-			max = value
-		}
-
-		if min == 0 {
-			min = value
-		}
-		if value < min {
-			min = value
-		}
-
-	}
-
-	if max == min {
-		result = strconv.Itoa(int(max))
-	} else {
-		result = strconv.Itoa(int(max))
-		result += " "
-		result += strconv.Itoa(int(min))
-
-	}
-
-	fmt.Println(result)
+	fmt.Println(DigitalRoot(493193))
 
 }
+
+func DigitalRoot(n int) int {
+
+	words := strings.Split(strconv.Itoa(n), "")
+
+	if len(words) == 1 {
+		return n
+	}
+
+	for len(words) > 1 {
+		sum := 0
+
+		for _, value := range words {
+			s, _ := strconv.Atoi(value)
+			sum += s
+		}
+
+		words = strings.Split(strconv.Itoa(sum), "")
+	}
+
+	result, _ := strconv.Atoi(words[0])
+	return result
+}
+
+// func sumSliceString(sl []string) int {
+// 	sum := 0
+// 	for _, value := range sl {
+// 		s, err := strconv.Atoi(value)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		sum += s
+
+// 	}
+
+// 	return sum
+// }
